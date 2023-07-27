7c690bfe214a7f2e3173af444e32bad01ef7460c
Pseurae <adhith.chand@gmail.com>
2023-07-28 00:39:49 +0530

    Make a weird Linking Cord sprite

--- src/data/item_icon_table.h
+++ src/data/item_icon_table.h
@@ -398,7 +398,7 @@ static const u32 *const sItemIconTable[ITEMS_COUNT + 1][2] =
     [ITEM_POWDER_JAR]     = {gItemIcon_PowderJar, gItemIconPalette_PowderJar},
     [ITEM_RUBY]           = {gItemIcon_Gem, gItemIconPalette_Ruby},
     [ITEM_SAPPHIRE]       = {gItemIcon_Gem, gItemIconPalette_Sapphire},
-    [ITEM_LINKING_CORD]   = {gItemIcon_TMCase, gItemIconPalette_TMCase},
+    [ITEM_LINKING_CORD]   = {gItemIcon_LinkingCord, gItemIconPalette_LinkingCord},
     // Return to field arrow
     [ITEMS_COUNT]         = {gItemIcon_ReturnToFieldArrow, gItemIconPalette_ReturnToFieldArrow},
 };
